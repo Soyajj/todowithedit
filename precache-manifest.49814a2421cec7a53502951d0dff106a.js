0c792a04eb5c856104e0896e045585225908ff1b
Soyaj Bajracharya <coolboysoyaz@gmail.com>
2020-08-30 18:04:59 +0545

    Updates

---
+++ precache-manifest.49814a2421cec7a53502951d0dff106a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4a082cf5c001140faa8fac87daf729",
+    "url": "/todowithedit/index.html"
+  },
+  {
+    "revision": "3b7fda3b6014f6774594",
+    "url": "/todowithedit/static/css/main.061e2d24.chunk.css"
+  },
+  {
+    "revision": "db9a1b4a9bfc583e4b71",
+    "url": "/todowithedit/static/js/2.762b7ac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todowithedit/static/js/2.762b7ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7fda3b6014f6774594",
+    "url": "/todowithedit/static/js/main.e8f72285.chunk.js"
+  },
+  {
+    "revision": "922dcd97d872e8d1e1e2",
+    "url": "/todowithedit/static/js/runtime-main.567ba98b.js"
+  }
+]);
